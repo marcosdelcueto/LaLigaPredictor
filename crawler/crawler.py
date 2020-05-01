44067c8f1d37108791f42c8047cba158cf786337
Marcos del Cueto <marcos.delcueto91@gmail.com>
2020-05-01 21:50:14 +0100

    Added options to consider variations of long player names when looking for them in sofifa. This allows for larger robustness for long and complicated player names

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.6
 # Marcos del Cueto
 import re
+import sys
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -8,7 +9,7 @@ from bs4 import BeautifulSoup
 data = []
 base_url = 'https://www.bdfutbol.com/en/p/p.php?id='
 # Loop over all indeces
-for id_index in range(25600,25605):
+for id_index in range(29721,29910):
     # Assign corresponding season
     if id_index >= 25600 and id_index <= 25979:
         Season = 2009
@@ -82,41 +83,16 @@ for id_index in range(25600,25605):
                 if test == 'Manager': break
                 # skip line with 'Substitutes' string
                 if test == 'Substitutes': continue
-                # if possible, transform string to int
+                # if possible, transform string to int, to ignore integers
                 try:
                     test = int(test)
                 except:
                     pass
+                is_this_int_with_extra_time = re.findall(r".+\+.+",str(test))
+                #if is_this_int_with_extra_time: print('I FOUND INT AT EXTRA TIME', test)
                 #print(test,type(test))
-                if type(test) == str:
+                if type(test) == str and not is_this_int_with_extra_time:
                     player_name.append(test)
-                    #####################
-                    #player_complete_name = re.findall(rf"{player_name}</a>",str(soup))
-                    #base_url = 'https://sofifa.com/players?keyword='
-                    #url = base_url + str(player_name)
-                    #res = requests.get(url)
-                    #html_page = res.content
-                    #soup = BeautifulSoup(html_page, 'html.parser')
-                    ##text = soup.find_all(text=True)
-                    ##print(soup)
-                    #for line2 in soup:
-                        ##print(line2)
-                        #player_id = re.findall(r"href=\"/player/[0-9]+",str(line2))
-                        #player_id = re.findall(r"[0-9]+",str(player_id))
-                        #if player_id: 
-                            #player_sofifa_id = player_id
-                    #if len(player_sofifa_id) == 1: 
-                        #print(player_name,player_sofifa_id)
-                    #elif len(player_sofifa_id) > 1: 
-                        #print('####################################################################')
-                        #print('ERROR: More than 1 possible ID when looking in sofifa for:', player_name)
-                        #print('####################################################################')
-                    #elif len(player_sofifa_id) < 1:
-                        #print('####################################################################')
-                        #print('ERROR: No ID found when looking in sofifa for:', player_name)
-                        #print('####################################################################')
-                    #break ### this is just for testing 1 players
-                    #####################
             counter = counter + 1
     #print(player_name)
     #print('### START bdfutbol ###')
@@ -126,17 +102,20 @@ for id_index in range(25600,25605):
     base_url_player_bdfutbol = "https://www.bdfutbol.com/en/j/j"
     for p in range(len(player_name)):
         for line in soup:
+            #print('newline:', line)
             player_bdfutbol_id = re.findall(rf"<a href=\"../j/j[0-9]+.html\">{player_name[p]}</a>",str(line))
+            #print('### test ###:',p, player_name[p], player_bdfutbol_id)
             player_bdfutbol_id = re.findall(r"[0-9]+",str(player_bdfutbol_id))
             if player_bdfutbol_id: 
-                #print(player_name[p], player_bdfutbol_id)
+                #print(p, player_name[p], player_bdfutbol_id)
                 player_id_bdfutbol.append(player_bdfutbol_id[0])
                 break
 
     # For each player
-    for p in range(len(player_name)):
-    #for p in range(30):
+    #for p in range(len(player_name)):
+    for p in range(1):
         # go to the player page on bdfutbol
+        #print('test',p,player_name)
         url_player_bdfutbol = base_url_player_bdfutbol + str(player_id_bdfutbol[p]) + ".html"
         #print('#########################3')
         #print('TEST url:',url_player_bdfutbol)
@@ -152,7 +131,7 @@ for id_index in range(25600,25605):
                 player_complete_name = re.findall(r",.+-",str(player_complete_name))
                 # if we get the complete name of the player:
                 if player_complete_name: 
-                    #print(player_name[p],"##",player_complete_name[0][2:-2])
+                    #print(p,player_name[p],"##",player_complete_name[0][2:-2])
                     player_name_complete.append(player_complete_name[0][2:-2])
 
     #print(player_name_complete)
@@ -166,30 +145,109 @@ for id_index in range(25600,25605):
     base_url_sofifa_search = 'https://sofifa.com/players?keyword='
     base_url_sofifa_player = 'https://sofifa.com/player/'
     # For each player, look in sofifa and get player info
+    counter_player=0
     for p in player_name_complete:
+        player_sofifa_id = None
         url = base_url_sofifa_search + str(p)
-        #print('TEST url:',url)
+        #print('TEST search in sofifa url:',url)
         res = requests.get(url)
         html_page = res.content
         soup = BeautifulSoup(html_page, 'html.parser')
         #text = soup.find_all(text=True)
         #print(soup)
         for line in soup:
+            #print(line)
             player_id = re.findall(r"href=\"/player/[0-9]+",str(line))
             player_id = re.findall(r"[0-9]+",str(player_id))
             if player_id:
                 player_sofifa_id = player_id
+        if player_sofifa_id == None:
+            #################################
+            # If no ID has been found when searching long name in sofifa, try removing last word of long name
+            if len(p.split()) > 1:
+                #print('long name split words:',p.split())
+                new_name=[]
+                for i in p.split():
+                    new_name.append(i)
+                new_name = new_name[0:-1]
+                medium_name=''
+                for i in new_name:
+                    medium_name = medium_name + ' ' + i
+                #print('try looking for', medium_name)
+                url = base_url_sofifa_search + str(medium_name)
+                #print('TEST search in sofifa url:',url)
+                res = requests.get(url)
+                html_page = res.content
+                soup = BeautifulSoup(html_page, 'html.parser')
+                for line in soup:
+                    #print(line)
+                    player_id = re.findall(r"href=\"/player/[0-9]+",str(line))
+                    player_id = re.findall(r"[0-9]+",str(player_id))
+                    #print(player_id)
+                    if player_id:
+                        player_sofifa_id = player_id
+                        #print('I got an ID this time:', player_sofifa_id)
+            if player_sofifa_id == None:
+            ################################        
+            #################################
+                # If no ID has been found when searching long name in sofifa, try removing 2nd word from last name
+                if len(p.split()) > 2:
+                    #print('long name split words:',p.split())
+                    new_name=[]
+                    for i in p.split():
+                        new_name.append(i)
+                    new_name2 = new_name[0:1] + new_name[2:]
+                    medium_name=''
+                    for i in new_name2:
+                        medium_name = medium_name + ' ' + i
+                    #print('try looking for', medium_name)
+                    url = base_url_sofifa_search + str(medium_name)
+                    #print('TEST search in sofifa url:',url)
+                    res = requests.get(url)
+                    html_page = res.content
+                    soup = BeautifulSoup(html_page, 'html.parser')
+                    for line in soup:
+                        #print(line)
+                        player_id = re.findall(r"href=\"/player/[0-9]+",str(line))
+                        player_id = re.findall(r"[0-9]+",str(player_id))
+                        #print(player_id)
+                        if player_id:
+                            player_sofifa_id = player_id
+                            #print('I got an ID this time:', player_sofifa_id)
+                if player_sofifa_id == None:
+                ################################        
+                    #################################
+                    # If no ID has been found when searching long name in sofifa, try with short name
+                    #print('try looking for', player_name[counter_player])
+                    url = base_url_sofifa_search + str(player_name[counter_player])
+                    #print('TEST search in sofifa url:',url)
+                    res = requests.get(url)
+                    html_page = res.content
+                    soup = BeautifulSoup(html_page, 'html.parser')
+                    for line in soup:
+                        #print(line)
+                        player_id = re.findall(r"href=\"/player/[0-9]+",str(line))
+                        player_id = re.findall(r"[0-9]+",str(player_id))
+                        #print(player_id)
+                        if player_id:
+                            player_sofifa_id = player_id
+                            #print('I got an ID this time:', player_sofifa_id)
+                    if player_sofifa_id == None:
+                    ################################        
+                        print('####################################################################')
+                        print('ERROR: No ID found when looking in sofifa for:', p)
+                        print('ERROR: No ID found when looking in sofifa for:', player_name[counter_player])
+                        print('####################################################################')
+                        sys.exit()
         if len(player_sofifa_id) == 1:
-            #print(p,player_sofifa_id)
+            #print('TEST sofifa',p,player_sofifa_id)
             pass
         elif len(player_sofifa_id) > 1:
             print('####################################################################')
-            print('ERROR: More than 1 possible ID when looking in sofifa for:', player_name)
+            print('ERROR: More than 1 possible ID when looking in sofifa for:', p)
             print('####################################################################')
-        elif len(player_sofifa_id) < 1:
-            print('####################################################################')
-            print('ERROR: No ID found when looking in sofifa for:', player_name)
-            print('####################################################################')
+            sys.exit()
+        counter_player = counter_player+1
 
         # Once we have one unique ID, get data from that player at corresponding time
         time_label = str(fifa_year) + "0001"
